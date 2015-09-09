9222a250e8f3e1b20bed1f4debaf54c97597f0ec
JJ505 <jjohnson101j@gmail.com>
2015-09-09 17:47:34 -0600

    remove workspace

--- src/Entity.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Jalen on 9/9/2015.
- */
-public abstract class Entity {
-
-  //has methods for movement
-  //intersection, bounding rectangle
-  //player, zombie, and master zombie extend this
-}
