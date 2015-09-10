ca86790c416a490341df8f0e01f26d07c6254f14
Christiaan Martinez <cmdm@unm.edu>
2015-09-10 09:36:43 -0600

    Added entity constructor

--- src/Entity.java
+++ src/Entity.java
@@ -5,8 +5,18 @@
  * intersection, bounding rectangle
  * player, zombie, and master zombie extend this
  */
-public abstract class Entity {
+public abstract class Entity
+{
   //posibly make these double?
-  int x,y;
+  private int x,y;
+  private int width;
+  private String type;
+
+  public Entity(String type, int x, int y)
+  {
+    this.type = type;
+    this.x = x;
+    this.y = y;
+  }
 
 }
