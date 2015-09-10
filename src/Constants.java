c071c332d31ee553d133f237fbb2750471c51046
Christiaan Martinez <cmdm@unm.edu>
2015-09-10 10:32:36 -0600

    testing constants

--- src/Constants.java
+++ src/Constants.java
@@ -9,6 +9,6 @@ public class Constants {
   static final int EXIT = 3;
   static final int SCORCHED_FLOOR = 5;
   static final int BLACKNESS = 6;
-
+  static final int HI = 1;
   static final int SIZE = 80;
 }
