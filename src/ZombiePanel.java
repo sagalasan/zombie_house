e6a3fd26746689805d370c81d12c7d8eeee324bb
sagalasan <sagalasan@users.noreply.github.com>
2015-09-13 16:28:54 -0600

    Added buffered image code

--- src/ZombiePanel.java
+++ src/ZombiePanel.java
@@ -1,3 +1,4 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -5,6 +6,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.Buffer;
 
 /**
  * Created by Jalen on 9/9/2015.
@@ -61,41 +65,51 @@ public class ZombiePanel extends JPanel implements KeyListener{
   public void paintComponent(Graphics g)
   {
     //this is just copy pasted code from conways GoL
-    BufferedImage floor = new BufferedImage(pixelSize, pixelSize, BufferedImage.TYPE_INT_ARGB);
-    Graphics2D floorDraw = floor.createGraphics();
-    floorDraw.setColor(Color.BLACK);
-    floorDraw.fillRect(0, 0, pixelSize, pixelSize);
-    floorDraw.setColor(Color.ORANGE);
-    floorDraw.fillRect(1, 1, pixelSize - 1, pixelSize - 1);
+    //BufferedImage floor = new BufferedImage(pixelSize, pixelSize, BufferedImage.TYPE_INT_ARGB);
+    //Graphics2D floorDraw = floor.createGraphics();
+    //floorDraw.setColor(Color.BLACK);
+    //floorDraw.fillRect(0, 0, pixelSize, pixelSize);
+    //floorDraw.setColor(Color.ORANGE);
+    //floorDraw.fillRect(1, 1, pixelSize - 1, pixelSize - 1);
+//
+    //BufferedImage wallBlock = new BufferedImage(pixelSize, pixelSize, BufferedImage.TYPE_INT_ARGB);
+    //Graphics2D wallDraw = wallBlock.createGraphics();
+    //wallDraw.setColor(Color.BLACK);
+    //wallDraw.fillRect(0, 0, pixelSize, pixelSize);
+    //wallDraw.setColor(Color.gray);
+    //wallDraw.fillRect(1, 1, pixelSize, pixelSize);
+//
+    //BufferedImage pathBlock = new BufferedImage(pixelSize-1, pixelSize-1, BufferedImage.TYPE_INT_ARGB);
+    //Graphics2D pathDraw = pathBlock.createGraphics();
+    //pathDraw.setColor(Color.BLACK);
+    //pathDraw.fillRect(0, 0, pixelSize-1, pixelSize-1);
+    //pathDraw.setColor(Color.gray);
+    //pathDraw.fillRect(1, 1, pixelSize-1, pixelSize-1);
+//
+    //for (int i = 0; i < Level.width; i++)
+    //{
+    //  for (int j = 0; j < Level.height; j++)
+    //  {
+    //    if (Level.map[i][j].type == Constants.FLOOR)
+    //    {
+    //      g.drawImage(floor, i * pixelSize, j * pixelSize, null);
+    //    }
+    //    else if (Level.map[i][j].type == Constants.WALL)
+    //    {
+    //      g.drawImage(wallBlock, i * pixelSize, j * pixelSize, null);
+    //    }
+    //  }
+    //}
 
-    BufferedImage wallBlock = new BufferedImage(pixelSize, pixelSize, BufferedImage.TYPE_INT_ARGB);
-    Graphics2D wallDraw = wallBlock.createGraphics();
-    wallDraw.setColor(Color.BLACK);
-    wallDraw.fillRect(0, 0, pixelSize, pixelSize);
-    wallDraw.setColor(Color.gray);
-    wallDraw.fillRect(1, 1, pixelSize, pixelSize);
-
-    BufferedImage pathBlock = new BufferedImage(pixelSize-1, pixelSize-1, BufferedImage.TYPE_INT_ARGB);
-    Graphics2D pathDraw = pathBlock.createGraphics();
-    pathDraw.setColor(Color.BLACK);
-    pathDraw.fillRect(0, 0, pixelSize-1, pixelSize-1);
-    pathDraw.setColor(Color.gray);
-    pathDraw.fillRect(1, 1, pixelSize-1, pixelSize-1);
-
-    for (int i = 0; i < Level.width; i++)
+    BufferedImage floor = null;
+    BufferedImage wall = null;
+    try
     {
-      for (int j = 0; j < Level.height; j++)
-      {
-        if (Level.map[i][j].type == Constants.FLOOR)
-        {
-          g.drawImage(floor, i * pixelSize, j * pixelSize, null);
-        }
-        else if (Level.map[i][j].type == Constants.WALL)
-        {
-          g.drawImage(wallBlock, i * pixelSize, j * pixelSize, null);
-        }
-      }
-    }
+      floor = ImageIO.read(new File("tile_images/zombie_house_tile_floor_test.png"));
+      wall = ImageIO.read(new File("tile_images/zombie_house_tile_wall_test.png"));
+    } catch (IOException e) {}
+
+
     g.setColor(Color.red);
     //System.out.println(gameController.userPlayer.x);
     g.fillOval(gameController.userPlayer.getX() * pixelSize, gameController.userPlayer.getY() * pixelSize, 30, 30);
