6ea3e11021ff42184d0b6ae004b00ee41323540b
lmirabal8689 <lmirabal8689@gmail.com>
2015-09-14 11:32:50 -0600

    Adding level generator code, Rectangle class

--- src/Level.java
+++ src/Level.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by Jalen on 9/9/2015.
  * @Level
@@ -8,10 +11,26 @@
  */
 
 public class Level {
+
+  final int HEIGHT = 40;
+  final int WIDTH = 35;
+
   static Tile[][] map;
   static int height = 30, width = 40;
 
+  Rectangle rt;
+
+  private Random rnd = new Random();
+
+  int[][] mapRooms;
+
+  ArrayList<Rectangle> rectangles = new ArrayList<Rectangle>();
+
+
   public Level(){
+
+
+
     //creates map, use helper methods later
     map = new Tile[width][height];
     for (int i = 0; i < width; i++)
@@ -28,6 +47,163 @@ public class Level {
         }
       }
     }
+
+
   }
+
+
+
+
+  public void mapGen()
+  {
+    rt = new Rectangle( 0, 0, HEIGHT, WIDTH); //
+    rectangles.add(rt); //populate rectangle store with root area
+    //number of leaves to create
+    while( rectangles.size() < rnd.nextInt(18)+14)
+    {
+      int splitIdx = rnd.nextInt( rectangles.size() ); // choose a random element
+      Rectangle toSplit = rectangles.get(splitIdx);
+      if( toSplit.split() )
+      { //attempt to split
+        rectangles.add( toSplit.leftChild );
+        rectangles.add( toSplit.rightChild );
+      }
+    }
+
+    //create the rooms
+    rt.createRoom();
+    //after rooms are created set everything
+    setGrid(rectangles);
+
+  }
+
+  public void setGrid(ArrayList<Rectangle> r) {
+    mapRooms = new int[HEIGHT][WIDTH];
+    int rooms = 2;
+
+    for (int i = 0; i < WIDTH; i++) {
+      for (int j = 0; j < HEIGHT; j++) {
+        mapRooms[j][i] = 0;
+      }
+    }
+
+    for (Rectangle rec : r)
+    {
+      if (rec.room != null)
+      {
+        for (int i = 0; i < rec.room.width; i++)
+        {
+          for (int j = 0; j < rec.room.height; j++)
+          {
+            mapRooms[rec.room.x + j][rec.room.y + i] = rooms;
+          }
+        }
+
+        rooms++;
+      }
+    }
+
+    //overload the panel with the rectangles
+    //mp.setRectangle(r);
+
+    for (Rectangle rec : r)
+    {
+      if (rec.room != null)
+      {
+        Rectangle c = findClosest(rec.room);
+        //System.out.println("closest = " + rec.room.x +" " + rec.room.y + " " + rec.room.height + "x" + rec.room.width);
+        //System.out.println("closest = " + c.room.x +" " + c.room.y + " " + c.room.height + "x" + c.room.width);
+
+        int firstX = rnd.nextInt((rec.room.width+rec.room.x) - rec.room.x)+rec.room.x;
+        int firstY = rnd.nextInt((rec.room.height+rec.room.y) - rec.room.y)+rec.room.y;
+
+        int secondX = rnd.nextInt((c.room.width+c.room.x) - c.room.x)+c.room.x;
+        int secondY = rnd.nextInt((c.room.height+c.room.y) - c.room.y)+c.room.y;
+
+        while(secondX != firstX || secondY != firstY)
+        {
+          System.out.println("firstX Y" + firstX + "," + firstY);
+          System.out.println("secondX Y" + secondX + "," + secondY);
+
+          if(secondX != firstX)
+          {
+            if(secondX > firstX)
+            {
+              secondX--;
+            }
+            else secondX++;
+          }
+          else if(secondY != firstY)
+          {
+            if(secondY > firstY)
+            {
+              secondY--;
+            }
+            else
+              secondY++;
+          }
+          mapRooms[secondX][secondY] = 1;
+        }
+      }
+
+    }
+
+    for (int i = 0; i < WIDTH; i++)
+    {
+      for (int j = 0; j < HEIGHT; j++)
+      {
+        System.out.print(map[j][i]);
+      }
+      System.out.println();
+    }
+
+    //overload the panel with the 2d array
+    //mp.setMap(map);
+
+  }
+
+  public Rectangle findClosest(Rectangle r)
+  {
+    int currentMidX = (r.width/2)+r.x;
+    int currentMidY = (r.height/2)+r.y;
+    int closestDistance = 100;
+    Rectangle closest = null;
+
+    for (Rectangle rec : rectangles)
+    {
+      if (rec.room != null)
+      {
+        if(r != rec.room)
+        {
+          int midX = (rec.room.width/2)+rec.room.x;
+          int midY = (rec.room.height/2)+rec.room.y;
+          //int distance = Math.abs(currentMidX - midX) + Math.abs(currentMidY - midY);
+          double distance = Math.sqrt(Math.pow((currentMidX - midX),2) + Math.pow((currentMidY - midY),2));
+          if(distance < closestDistance)
+          {
+            closestDistance = (int)distance;
+            closest = rec;
+          }
+        }
+      }
+    }
+    return closest;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
---
+++ src/Rectangle.java
@@ -0,0 +1,92 @@
+import java.util.Random;
+
+/**
+ * Created by laurencemirabal on 9/14/15.
+ */
+public class Rectangle {
+    private static int MIN_ROOM_SIZE = 7;
+    private static Random rnd = new Random();
+
+    int x, y, width, height;
+    Rectangle leftChild;
+    Rectangle rightChild;
+    Rectangle room;
+    Rectangle hall;
+
+    public Rectangle(int x, int y, int height, int width)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public boolean split()
+    {
+        //if area already split then do nothing
+        if( leftChild != null )
+        {
+            return false;
+        }
+        //generate either true or false for the split direction
+        boolean horizontal = rnd.nextBoolean();
+
+        //if horizontal is true we precede with height, otherwise we precede with width
+        //and check that the room size will fit
+        int max = (horizontal ? height : width ) - MIN_ROOM_SIZE;
+
+        if( max <= MIN_ROOM_SIZE )
+        {
+            return false;
+        }
+        int split = rnd.nextInt( max ); // generate split point
+
+        //check that the split size can accommodate the min room size
+        if(split < MIN_ROOM_SIZE)
+        {
+            split = MIN_ROOM_SIZE;
+        }
+        //if horizontal is true then we split horizontally and make the child areas
+        if(horizontal)
+        {
+            leftChild = new Rectangle( x, y, split, width );
+            rightChild = new Rectangle( x+split, y, height-split, width );
+        }
+        //if horizontal is not true then we split vertically
+        else
+        {
+            leftChild = new Rectangle( x, y, height, split );
+            rightChild = new Rectangle( x, y+split, height, width-split );
+        }
+
+        //we split
+        return true;
+    }
+
+    public void createRoom()
+    {
+        //if leftChild is not null we have a documented area so there should already
+        //be a room in that location
+        if(leftChild != null)
+        {
+            leftChild.createRoom();
+            rightChild.createRoom();
+        }
+        //if there is no room than create one based on the leaf nodes available space
+        //and also with the constraints set.
+        else
+        {   //if height - MIN_ROOM_SIZE <= 0, we only have enough room for the min room
+            //size. Otherwise we pick a number within the areas size + min room size.
+            //this also determines half the height to be joined with the leaf
+            int roomTop = (height - MIN_ROOM_SIZE <= 0) ? 0 : rnd.nextInt( height - MIN_ROOM_SIZE);
+            //this variable is the same as the one above but is for determining room width
+            int roomLeft =  (width - MIN_ROOM_SIZE <= 0) ? 0 : rnd.nextInt( width - MIN_ROOM_SIZE);
+            //now we set those constraints into what will become the dimensions of the room
+            int roomHeight = Math.max(rnd.nextInt( height - roomTop ), MIN_ROOM_SIZE );
+            int roomWidth = Math.max(rnd.nextInt( width - roomLeft ), MIN_ROOM_SIZE );
+            //create the room object
+            room = new Rectangle( x + roomTop, y+roomLeft, roomHeight, roomWidth);
+        }
+    }
+
+}
